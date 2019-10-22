44da2429e45fb51d903c19eb64b91933bac940fa
Furqan Aliyuddien <furqanaliyuddien@gmail.com>
2019-10-22 11:19:27 +0700

    admin page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', 'pagesController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('admin/home', 'HomeController@adminHome')->name('admin.home')->middleware('is_admin');
